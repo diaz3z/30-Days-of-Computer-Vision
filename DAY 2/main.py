10e140754befb67ecac7dd8d6f372c2be253464e
Mohammad Zaid <mdzaid786291@gmail.com>
2024-09-21 19:17:09 +0530

    Day 2 done:

--- DAY 2/main.py
+++ DAY 2/main.py
@@ -1,14 +1,27 @@
 import cv2
-import util
+from PIL import Image
+from util import get_limits
 
 cap = cv2.VideoCapture(0)
+
+yellow = [0, 255, 255]
 
 while True:
     ret, frame = cap.read()
 
-    hsvImg = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    hsvImg = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    mask = cv2.inRange(hsvImg, ())
+    lowerLimit, upperLimit = get_limits(color=yellow)
+
+    mask = cv2.inRange(hsvImg, lowerLimit, upperLimit)
+
+    mask_ = Image.fromarray(mask)
+
+    bbox = mask_.getbbox()
+
+    if bbox is not None:
+        x1, y1, x2, y2 = bbox
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
 
     cv2.imshow("frame", frame)
 
