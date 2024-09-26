ed2b0ff3e87bbcc3b1f4aa7008d32f0017311e28
Mohammad Zaid <mdzaid786291@gmail.com>
2024-09-26 20:07:20 +0530

    testing face emotion classifier

--- DAY 6/prepare_data.py
+++ DAY 6/prepare_data.py
@@ -1,12 +1,22 @@
 from utils import get_face_landmarks
 import os
 import cv2
+import numpy as np
 
+data_dir = "DAY 6\data"
 
-data_dir = "./data"
+output = []
 
-for emotion in os.listdir(data_dir):
+for emotion_indx, emotion in enumerate(os.listdir(data_dir)):
     for image_path in os.listdir(os.path.join(data_dir, emotion)):
         image_path = os.path.join(data_dir, emotion, image_path)
 
-        image_rows, image_cols, _ = 
+        image = cv2.imread(image_path)
+
+        face_landmarks = get_face_landmarks(image)
+
+        if len(face_landmarks) == 1404:
+            face_landmarks.append(int(emotion_indx))
+            output.append(face_landmarks)
+            
+np.savetxt(os.path.join(os.path.dirname(__file__), "data.txt"), np.asarray(output))
